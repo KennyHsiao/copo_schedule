94d05d506710bb66aa15f20363925502653341a4
allen@e-pay.vip <allen@e-pay.vip>
2022-12-20 16:00:01 +0800

    update  CommissionMonthReport

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func main() {
 	)
 
 	// (計算月傭金報表Schedule) 每月2號 03:00:00執行
-	c.AddJob("0 0/1 * * * ?",
+	c.AddJob("0 0 3 2 * ?",
 		cron.NewChain(cron.SkipIfStillRunning(logger)).
 			Then(&cronjob.CommissionMonthReport{}),
 	)
