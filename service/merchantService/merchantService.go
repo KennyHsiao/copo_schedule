7991ef8c32895a40d5fbc3768d1e4ac588f27cf6
Kenny <kenny@e-pay.vip>
2023-01-09 15:40:00 +0800

    fix merchant callback errorNote for PHP

--- service/merchantService/merchantService.go
+++ service/merchantService/merchantService.go
@@ -39,7 +39,7 @@ func PostCallbackToMerchant(db *gorm.DB, context *context.Context, orderX *types
 	ProxyPayCallBackMerRespVO.Set("orderAmount", fmt.Sprintf("%.2f", orderX.OrderAmount))
 	ProxyPayCallBackMerRespVO.Set("fee", fmt.Sprintf("%.2f", orderX.Fee))
 	ProxyPayCallBackMerRespVO.Set("payOrderTime", orderX.TransAt.Time().Format("200601021504"))
-	if orderX.MerchantCode == "ME00335" {
+	if orderX.CurrencyCode == "PHP" {
 		ProxyPayCallBackMerRespVO.Set("errorNote", orderX.ErrorNote)
 	}
 
