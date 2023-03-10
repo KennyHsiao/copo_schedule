c4b6d5477cce92eafc960a6fdc80d27a7e8b2794
allen@e-pay.vip <allen@e-pay.vip>
2023-03-10 15:55:38 +0800

    fix order for

--- cronjob/proxypayjob.go
+++ cronjob/proxypayjob.go
@@ -33,7 +33,6 @@ func (l *ProxyToChannel) Run() {
 
 	logx.WithContext(l.ctx).Infof("执行时间：%s", time.Now().Format("2006-01-02 15:04:05"))
 
-	p := service.NewProxyPayEvent(l.ctx)
 	if err := helper.COPO_DB.Table("tx_orders").Where("`type` = ? AND `status` = ? ", constants.ORDER_TYPE_DF, constants.WAIT_PROCESS).
 		Where("TIMEDIFF(CURRENT_TIMESTAMP(), TIMESTAMPADD(MINUTE,480,DATE_FORMAT(created_at,'%Y-%m-%d %T'))) < 300").
 		Find(&orders).Error; err != nil {
@@ -48,90 +47,93 @@ func (l *ProxyToChannel) Run() {
 		for _, order := range orders {
 			//没有回调渠道订单号，以及渠道回调时间/交易时间，才会发送到渠道
 			if order.ChannelOrderNo == "" && order.ChannelCallBackAt.IsZero() && order.TransAt.Time().IsZero() {
-				channel := types.ChannelData{}
-				if queryErr := helper.COPO_DB.Table("ch_channels").Where("code = ?", order.ChannelCode).Find(&channel); queryErr != nil {
-					logx.WithContext(l.ctx).Error("queryErr: \n", queryErr)
-				}
-
-				url := fmt.Sprintf("%s:%s/api/proxy-pay-query", viper.Get("CHANNEL_HOST"), channel.ChannelPort)
-				logx.WithContext(l.ctx).Infof("發送代付處理請求To渠道: %+v。 url: %s\n", order, url)
-
-				//異步調用-呼叫異步調用服務
-				resp := &vo.ProxyQueryRespVO{}
-				var err error
-				go func() {
-					if resp, err = p.AsyncProxyQueryEvent(&l.ctx, url, &order, wg); err != nil {
-						logx.WithContext(l.ctx).Errorf("orderNo: %s ,resp: %s\n", order.OrderNo, err)
-					}
-					logx.WithContext(l.ctx).Infof("resp:%+v\n", resp)
-
-					if err != nil || resp.Code != "0" { ////回复失败，都列为失败单，不管是否为网路异常....等
-						//1. 处理渠道返回错误讯习
-						if resp != nil {
-							logx.WithContext(l.ctx).Errorf("代付提单: %s ，渠道交易失败讯息: %s\n", order.OrderNo, resp.Message)
-							order.Status = constants.FAIL
-							order.RepaymentStatus = constants.REPAYMENT_WAIT
-							order.ErrorType = "1" //1.渠道返回错误	2.渠道异常	3.商户参数错误	4.账户为黑名单	5.其他
-							order.ErrorNote = "Code:" + resp.Code + " Message: " + resp.Message
-						}
-
-						if err != nil {
-							logx.WithContext(l.ctx).Errorf("代付提单: %s ，渠道交易失败讯息Err: %s\n", order.OrderNo, err.Error())
-							order.Status = constants.FAIL
-							order.RepaymentStatus = constants.REPAYMENT_WAIT
-							order.ErrorType = "1" //1.渠道返回错误	2.渠道异常	3.商户参数错误	4.账户为黑名单	5.其他
-							order.ErrorNote += " Err: " + err.Error()
-						}
-
-						// 2. 处理还款
-						var resRpc *transaction.ProxyPayFailResponse
-						var errRpc error
-						//呼叫RPCc还款
-						balanceType, errBalance := merchantService.GetBalanceType(helper.COPO_DB, order.ChannelCode, order.Type)
-						if errBalance != nil {
-							logx.WithContext(l.ctx).Errorf("BalanceType Err: %s\n", errBalance.Error())
-						}
-						rpc := helper.TransactionRpc
-						if balanceType == "DFB" {
-							resRpc, errRpc = rpc.ProxyOrderTransactionFail_DFB(context.Background(), &transaction.ProxyPayFailRequest{
-								MerchantCode: order.MerchantCode,
-								OrderNo:      order.OrderNo,
-							})
-						} else if balanceType == "XFB" {
-							resRpc, errRpc = rpc.ProxyOrderTransactionFail_XFB(context.Background(), &transaction.ProxyPayFailRequest{
-								MerchantCode: order.MerchantCode,
-								OrderNo:      order.OrderNo,
-							})
-						}
-
-						if errRpc != nil {
-							logx.WithContext(l.ctx).Errorf("代付提单 %s 还款失败。 Err: %s\n", order.OrderNo, errRpc.Error())
-							order.RepaymentStatus = constants.REPAYMENT_FAIL
-
-							// 更新订单
-							if errUpdate := helper.COPO_DB.Table("tx_orders").Updates(order).Error; errUpdate != nil {
-								logx.WithContext(l.ctx).Errorf("代付订单更新状态错误: %s\n", errUpdate.Error())
-							}
-
-						} else {
-							logx.WithContext(l.ctx).Infof("2代付還款rpc完成，%s 錢包還款完成: %#v\n", balanceType, resRpc)
-							order.RepaymentStatus = constants.REPAYMENT_SUCCESS
-						}
-
-					} else {
-						//若成功则更新交易中、無須還款
-						order.Status = constants.TRANSACTION
-						order.RepaymentStatus = constants.REPAYMENT_NOT
-					}
-
-					// 更新订单
-					if errUpdate := helper.COPO_DB.Table("tx_orders").Updates(&order).Error; errUpdate != nil {
-						logx.WithContext(l.ctx).Error("代付订单更新状态错误: \n", errUpdate.Error())
-					}
-				}()
+				l.doAsyncProxyQueryEvent(order, wg)
 			}
 		}
 		wg.Wait()
 		logx.WithContext(l.ctx).Info("WaitGroup Finished")
 	}
+}
+
+func (l *ProxyToChannel) doAsyncProxyQueryEvent(order types.OrderX, wg *sync.WaitGroup) {
+	//異步調用-呼叫異步調用服務
+	go func() {
+		resp := &vo.ProxyQueryRespVO{}
+		var err error
+		channel := types.ChannelData{}
+		if queryErr := helper.COPO_DB.Table("ch_channels").Where("code = ?", order.ChannelCode).Find(&channel); queryErr != nil {
+			logx.WithContext(l.ctx).Error("queryErr: \n", queryErr)
+		}
+		url := fmt.Sprintf("%s:%s/api/proxy-pay-query", viper.Get("CHANNEL_HOST"), channel.ChannelPort)
+		logx.WithContext(l.ctx).Infof("發送代付處理請求To渠道: %+v。 url: %s\n", order, url)
+		p := service.NewProxyPayEvent(l.ctx)
+		if resp, err = p.AsyncProxyQueryEvent(&l.ctx, url, &order, wg); err != nil {
+			logx.WithContext(l.ctx).Errorf("orderNo: %s ,resp: %s\n", order.OrderNo, err)
+		}
+		logx.WithContext(l.ctx).Infof("resp:%+v\n", resp)
+
+		if err != nil || resp.Code != "0" { ////回复失败，都列为失败单，不管是否为网路异常....等
+			//1. 处理渠道返回错误讯习
+			if resp != nil {
+				logx.WithContext(l.ctx).Errorf("代付提单: %s ，渠道交易失败讯息: %s\n", order.OrderNo, resp.Message)
+				order.Status = constants.FAIL
+				order.RepaymentStatus = constants.REPAYMENT_WAIT
+				order.ErrorType = "1" //1.渠道返回错误	2.渠道异常	3.商户参数错误	4.账户为黑名单	5.其他
+				order.ErrorNote = "Code:" + resp.Code + " Message: " + resp.Message
+			}
+
+			if err != nil {
+				logx.WithContext(l.ctx).Errorf("代付提单: %s ，渠道交易失败讯息Err: %s\n", order.OrderNo, err.Error())
+				order.Status = constants.FAIL
+				order.RepaymentStatus = constants.REPAYMENT_WAIT
+				order.ErrorType = "1" //1.渠道返回错误	2.渠道异常	3.商户参数错误	4.账户为黑名单	5.其他
+				order.ErrorNote += " Err: " + err.Error()
+			}
+
+			// 2. 处理还款
+			var resRpc *transaction.ProxyPayFailResponse
+			var errRpc error
+			//呼叫RPCc还款
+			balanceType, errBalance := merchantService.GetBalanceType(helper.COPO_DB, order.ChannelCode, order.Type)
+			if errBalance != nil {
+				logx.WithContext(l.ctx).Errorf("BalanceType Err: %s\n", errBalance.Error())
+			}
+			rpc := helper.TransactionRpc
+			if balanceType == "DFB" {
+				resRpc, errRpc = rpc.ProxyOrderTransactionFail_DFB(context.Background(), &transaction.ProxyPayFailRequest{
+					MerchantCode: order.MerchantCode,
+					OrderNo:      order.OrderNo,
+				})
+			} else if balanceType == "XFB" {
+				resRpc, errRpc = rpc.ProxyOrderTransactionFail_XFB(context.Background(), &transaction.ProxyPayFailRequest{
+					MerchantCode: order.MerchantCode,
+					OrderNo:      order.OrderNo,
+				})
+			}
+
+			if errRpc != nil {
+				logx.WithContext(l.ctx).Errorf("代付提单 %s 还款失败。 Err: %s\n", order.OrderNo, errRpc.Error())
+				order.RepaymentStatus = constants.REPAYMENT_FAIL
+
+				// 更新订单
+				if errUpdate := helper.COPO_DB.Table("tx_orders").Updates(order).Error; errUpdate != nil {
+					logx.WithContext(l.ctx).Errorf("代付订单更新状态错误: %s\n", errUpdate.Error())
+				}
+
+			} else {
+				logx.WithContext(l.ctx).Infof("2代付還款rpc完成，%s 錢包還款完成: %#v\n", balanceType, resRpc)
+				order.RepaymentStatus = constants.REPAYMENT_SUCCESS
+			}
+
+		} else {
+			//若成功则更新交易中、無須還款
+			order.Status = constants.TRANSACTION
+			order.RepaymentStatus = constants.REPAYMENT_NOT
+		}
+
+		// 更新订单
+		if errUpdate := helper.COPO_DB.Table("tx_orders").Updates(&order).Error; errUpdate != nil {
+			logx.WithContext(l.ctx).Error("代付订单更新状态错误: \n", errUpdate.Error())
+		}
+	}()
 }
